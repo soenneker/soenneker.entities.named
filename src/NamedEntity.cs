c039df3456f108c65aaaf03ed06307baa3005d92
Jake Soenneker <jake@soenneker.com>
2025-08-16 13:05:05 -0500

    no longer abstract

--- src/NamedEntity.cs
+++ src/NamedEntity.cs
@@ -5,9 +5,9 @@ using Soenneker.Entities.Named.Abstract;
 namespace Soenneker.Entities.Named;
 
 /// <inheritdoc cref="INamedEntity"/>
-public abstract class NamedEntity : Entity.Entity, INamedEntity
+public class NamedEntity : Entity.Entity, INamedEntity
 {
     [JsonPropertyName("name")]
     [JsonProperty("name")]
-    public string Name { get; set; } = default!;
+    public virtual string Name { get; set; } = null!;
 }
